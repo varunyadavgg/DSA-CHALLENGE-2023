bb649778728272b0294a25c670f4b2658d5b7248
varunyadavgg <zvarun747@gmail.com>
2023-07-04 22:10:08 +0530

    updated 5

---
+++ arrays/easy/287/287. Find the Duplicate Number.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return nums[i]
+        return -1
